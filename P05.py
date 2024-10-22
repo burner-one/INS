9eabb0aad81c1bee96806cd92d108ae281a1c263
jack napier <freddiequellfreddie@gmail.com>
2024-10-22 11:05:45 +0530

    Update P05.py

--- P05.py
+++ P05.py
@@ -1,53 +1,41 @@
-# Diffie-Hellman Code  
-  
-# Power function to return value of a^b mod P  
-  
-def power(a, b, p):  
-  
-if b == 1:  
-  
-return a  
-  
-else:  
-  
-return pow(a, b) % p  
-  
-# Main function  
-  
-def main():  
-  
-#Both persons agree upon the public keys G and P  
-  
-# A prime number Pis taken  
-  
-P = 23  
-  
-print("The value of P:", P)  
-  
-# A primitive root for P, Gis taken  
-  
-G=9  
-  
-print("The value of G:", G)  
-  
-# Alice chooses the private key a  
-  
-#a is the chosen private key  
-  
-a = 4  
-print("The private key a for Alice:", a)  
-# Gets the generated key  
-INS x = power (G, a, P)  
-# Bob chooses the private key b  
-#b is the chosen private key  
-b = 3  
-print("The private key b for Bob:", b)  
-#Gets the generated key  
-y=power (G, b, P)  
-# Generating the secret key after the exchange of keys  
-ka=power(y, a, P) # Secret key for Alice  
-kb=power(x, b, P) # Secret key for Bob  
-print("Secret key for Alice is:", ka)  
-print("Secret key for Alice is:", ka)  
-if __name__=="__main__":  
-main()  
+# Diffie-Hellman Code
+
+# Power function to return value of a^b mod P
+def power(a, b, p):
+    if b == 1:
+        return a % p
+    else:
+        return pow(a, b, p)
+
+# Main function
+def main():
+    # Both persons agree upon the public keys G and P
+    P = 23  # A prime number
+    print("The value of P:", P)
+
+    G = 9  # A primitive root for P
+    print("The value of G:", G)
+
+    # Alice chooses the private key a
+    a = 4  # Alice's private key
+    print("The private key a for Alice:", a)
+
+    # Generate the public key for Alice
+    x = power(G, a, P)
+
+    # Bob chooses the private key b
+    b = 3  # Bob's private key
+    print("The private key b for Bob:", b)
+
+    # Generate the public key for Bob
+    y = power(G, b, P)
+
+    # Generating the secret key after the exchange of keys
+    ka = power(y, a, P)  # Secret key for Alice
+    kb = power(x, b, P)  # Secret key for Bob
+
+    print("Secret key for Alice is:", ka)
+    print("Secret key for Bob is:", kb)
+
+if __name__ == "__main__":
+    main()
