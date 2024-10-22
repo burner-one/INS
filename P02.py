a54716444114fb43ba7bad4c88981d6adfbf37fa
jack napier <freddiequellfreddie@gmail.com>
2024-10-22 11:03:06 +0530

    Update P02.py

--- P02.py
+++ P02.py
@@ -1,73 +1,43 @@
 import math  
-  
-def gcd(a,h):  
-  
-temp = 0  
-  
-while(1):  
-  
-temp = a%h  
-  
-if (temp==0):  
-  
-return h  
-  
-a = h  
-  
-h = temp  
-  
+
+def gcd(a, h):
+    while True:
+        temp = a % h
+        if temp == 0:
+            return h
+        a = h
+        h = temp
+
+# Define prime numbers
 p = 3  
-  
 q = 7  
-  
-n = p*q  
-  
+n = p * q  
+
+# Initialize e
 e = 2  
-  
-phi = (p-1)*(q-1)  
-  
-while (e<phi):  
-  
-# e must be co-prime to phi and smaller than phi  
-  
-if (gcd(e,phi)==1):  
-  
-break  
-  
- 
- 
-else :  
-  
-e = e + 1  
-  
-#Private key (d stands for decrypt)  
-  
-#choosing d such that it satisfies  
-  
-# d*e = 1 + k * totient  
-  
+phi = (p - 1) * (q - 1)  
+
+# Find e that is co-prime to phi
+while e < phi:
+    if gcd(e, phi) == 1:
+        break
+    else:
+        e += 1
+
+# Calculate private key (d)
 k = 2  
-  
-d = (1+(k*phi))/e  
-  
-#Messsage to be encrypted  
-  
+d = (1 + (k * phi)) / e  
+
+# Message to be encrypted
 msg = 12.0  
-  
-print("Message data = ",msg)  
-  
-#Encryption c = msg(msg ^ e) % n  
-  
-c = pow(msg,e)  
-  
-c = math.fmod(c,n)  
-  
-print("Encrypted data = ", c)  
-  
-#Decryption m = (c ^ d) % n  
-  
-m = pow(c,d)  
-  
-m = math.fmod(m,n)  
-  
-print("Original message sent = ",m) 
+print("Message data =", msg)  
+
+# Encryption: c = (msg ^ e) % n
+c = pow(msg, e)  
+c = math.fmod(c, n)  
+print("Encrypted data =", c)  
+
+# Decryption: m = (c ^ d) % n
+m = pow(c, d)  
+m = math.fmod(m, n)  
+print("Original message sent =", m)  
