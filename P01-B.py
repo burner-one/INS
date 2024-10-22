fbab58a3c09395cf2fb61e5c2014998c40f2cbfc
jack napier <freddiequellfreddie@gmail.com>
2024-10-22 11:01:12 +0530

    Update P01-B.py

--- P01-B.py
+++ P01-B.py
@@ -1,18 +1,20 @@
-string = input("enter a string : ")  
-  
-def RailFence(txt):  
-  
-result = " "  
-  
-for i in range(len(string)):  
-  
-if(i%2==0):  
- result +=string[i]  
-  
-for i in range(len(string)):  
-  
-if(i%2!=0):  
- result +=string[i]  
- return result  
-  
-print(RailFence(string)) 
+# Get input from the user
+string = input("Enter a string: ")
+
+def RailFence(txt):
+    result = ""
+    
+    # Append characters at even indices
+    for i in range(len(txt)):
+        if i % 2 == 0:
+            result += txt[i]
+    
+    # Append characters at odd indices
+    for i in range(len(txt)):
+        if i % 2 != 0:
+            result += txt[i]
+    
+    return result
+
+# Print the encrypted result
+print("Encrypted text:", RailFence(string))
