f6dbb3a5d128fa350e94afc54af9fc97cc8a7abd
jack napier <freddiequellfreddie@gmail.com>
2024-10-22 11:01:55 +0530

    Update P01-A.py

--- P01-A.py
+++ P01-A.py
@@ -1,35 +1,24 @@
-def encrypt(text,s):  
-  
-result = " "  
-  
-#traverse text  
-  
-for i in range(len(text)):  
-  
-char = text[i]  
-  
-#Encrypt uppercase characters  
-  
-if (char.isupper()):  
-  
-result += chr((ord(char)+ s-65) % 26 + 65)  
-  
-#Envrypt Lowercase characters  
-  
-else:  
-  
-result += chr((ord(char)+ s-97) % 26 + 97)  
-  
-return result  
-  
-#check the above function  
-  
-text=input("Enter the text to encrypt ")  
-  
-s = 3  
-  
-print("Text : " + text)  
-  
-str(s)  
-  
-print("Cipher : " + encrypt(text,s)) 
+def encrypt(text, s):
+    result = ""
+    
+    # Traverse the text
+    for i in range(len(text)):
+        char = text[i]
+        
+        # Encrypt uppercase characters
+        if char.isupper():
+            result += chr((ord(char) + s - 65) % 26 + 65)
+        # Encrypt lowercase characters
+        elif char.islower():
+            result += chr((ord(char) + s - 97) % 26 + 97)
+        else:
+            result += char  # Preserve non-alphabetic characters
+    
+    return result
+
+# Check the above function
+text = input("Enter the text to encrypt: ")
+s = 3
+
+print("Text: " + text)
+print("Cipher: " + encrypt(text, s))
